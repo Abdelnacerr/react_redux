b6f23a9924b25e7744a5945a23fd1afddb8988ef
Abdelnacerr <abdelnacerr@gmail.com>
2021-07-10 20:51:18 +1000

    deleted unnecessary files

---
+++ src/reducers/counter.js
@@ -0,0 +1,15 @@
+
+//Reducer How your actions transform your state
+//into the next state. Modifies store/state
+const counterReducer = (state = 0, action) => {
+	switch (action.type) {
+		case "INCREMENT":
+			return state + action.payload;
+		case "DECREMENT":
+			return state - action.payload;
+		default:
+			return state;
+	}
+};
+
+export default counterReducer;
